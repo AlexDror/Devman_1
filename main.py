3ebbb608900097fa6dd89f9c2f38c4ba895090fe
AlexDror <vorobyoff@gmail.com>
2022-11-22 14:34:25 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 """
-Lesson 1. Weather forecast in console.
+В данном конкретном задании нет необходимости отдельно выделять функцию
+печати для каждого города, сделать это не сложно, но бессмыслено.
+Можно и рекурсивную функцию написать, и однострок вообще - все это лишь усложнит
+восприятие кода.
+Но если требование по выделению функции останется - ладно, выделю.
 """
-import requests
-from requests.exceptions import HTTPError
-
-
 LOCATIONS: tuple = ('London', 'SVO', 'Череповец')
-PARAMS: str = 'm3nqT&lang=ru'
-URL: str = 'http://wttr.in/'
+PARAMS: dict = {'m3nqT': '', 'lang': 'ru'}
+URL: str = 'http://wttr.in'
+
+
+from requests import get, Response
+from requests.exceptions import HTTPError, Timeout
 
 
 def print_weather() -> None:
@@ -16,12 +20,14 @@ def print_weather() -> None:
     """
     for location in LOCATIONS:
         try:
-            response: requests.Response = requests.get(URL + location, params=PARAMS, timeout=100)
+            response: Response = get(f'{URL}/{location}', params=PARAMS, timeout=20)
             response.raise_for_status()
+
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
-        except Exception as err:
-            print(f'Other error occurred: {err}')
+        except Timeout as timeout_error:
+            print(f'Website is down temporary... {timeout_error}')
+            print('Please try later')
         else:
             print(response.text)
 
